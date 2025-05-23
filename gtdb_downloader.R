a12a3f2ce82e0cb0b38be2f9fbeeb64069cc4e56
AggressiveHayBale <mateusz.jun@gmail.com>
2025-05-23 14:30:36 +0200

    Path for dataset, database optimization, better species input, bugfixes

--- gtdb_downloader.R
+++ gtdb_downloader.R
@@ -8,6 +8,13 @@ suppressMessages(
         }
 )
 
+#################
+## Update info ##
+#################
+## In case of database update please modify just the line annotated with following text
+## (if no column or parameters were modified):
+# NOTE: DATABASE UPDATE MODIFY JUST THIS LINES
+
 ### Parameters
 args <- commandArgs(trailingOnly = TRUE)
 
@@ -20,10 +27,12 @@ output_dir <- "output"
 domain <- "bacteria"
 database_file <- NULL
 non_interactive <- FALSE
+dataset_path <- NULL
+report <- FALSE
 
-#################
-## NOTE: 1. SRC##
-#################
+##################
+## NOTE: 1. SRC ##
+##################
 green <- function(x) paste0("\033[1;32m", x, "\033[0m")
 yellow <- function(x) paste0("\033[1;33m", x, "\033[0m")
 red <- function(x) paste0("\033[1;31m", x, "\033[0m")
@@ -39,12 +48,22 @@ tax_split <- function(dt) {
         return(dt)
 }
 
+tax_split_ncbi <- function(dt) {
+        ## tax split
+        tax_levels <- c("ncbi_phylium", "ncbi_class", "ncbi_order", "ncbi_family", "ncbi_genus", "ncbi_species")
+        dt[, (tax_levels) := tstrsplit(ncbi_taxonomy, ";")[2:7]]
+        ## Pref remove
+        for (col in tax_levels) {
+                dt[, (col) := sub("^[a-z]__", "", get(col))]
+        }
+        return(dt)
+}
 
 
-# Parse named arguments
+# Parse parameters arguments
 i <- 1
 while (i <= length(args)) {
-        if (args[i] %in% c("--name_selection", "-n")) {
+        if (args[i] %in% c("--name", "--name_selection", "-n")) {
                 name_selection <- args[i + 1]
                 i <- i + 2
         } else if (args[i] %in% c("--taxrank", "-t")) {
@@ -71,19 +90,27 @@ while (i <= length(args)) {
         } else if (args[i] %in% c("--non_interactive", "-ni")) {
                 non_interactive <- TRUE
                 i <- i + 2
+        } else if (args[i] %in% c("--report", "-r")) {
+                report <- TRUE
+                i <- i + 2
+        } else if (args[i] %in% c("--dataset_path ", "-dp")) {
+                dataset_path <- args[i + 1]
+                i <- i + 2
         } else if (args[i] %in% c("--help", "-h")) {
                 cat("Usage: fasta_extractor [options]\n")
                 cat("Example command:\n")
                 cat('./gtdb_downloader.R -n "Pseudomonas aeruginosa" \n')
                 cat("Options:\n")
-                cat("  -n, --name_selection <name>   Name of the taxon to select (required; eg. Escherichia coli)\n")
+                cat("  -n, --name <name>   Name of the taxon to select based on GTDB (preferable), NCBI taxonomy or NCBI taxid (required; eg. Escherichia coli)\n")
                 cat("  -t, --taxrank <rank>         Taxonomic rank [default: species]\n")
                 cat("  -r, --representative_check <t/f> Check for representative genomes [default: t]\n")
                 cat("  -d, --dryrun                 Dry run (no actual processing) [default: FALSE]\n")
                 cat("  -o, --output_dir <dir>       Output directory [default: output]\n")
                 cat("  -m, --domain <domain>        Domain (bacteria or archaea) [default: bacteria]\n")
                 cat("  -db, --database <file>       Custom database file path\n")
+                cat("  -dp, --dataset_path          Path to dataset tool (not required)\n")
                 cat("  -ni, --non_interactive       Do not ask for confirmation before downloading genomes\n")
+                cat("  -r, --report                 Create a report with each sample accession number and taxonomy\n")
                 cat("  -h, --help                   Show this help message\n")
                 quit()
         } else {
@@ -99,9 +126,9 @@ while (i <= length(args)) {
         }
 }
 
-# Check if name_selection was givven
+# Check if name_selection was given
 if (is.null(name_selection)) {
-        stop(red("Error: --name_selection argument is required\nUse --help for usage information"), call. = FALSE)
+        stop(red("--name argument is required\nUse --help for usage information"), call. = FALSE)
 }
 ############################
 ## NOTE: 2. Tool download ##
@@ -125,14 +152,23 @@ if (!is.null(database_file)) {
                 default_files <- c("ar53_metadata_r226.tsv.gz", "ar53_metadata_r226.tsv")
                 download_url <- "https://data.ace.uq.edu.au/public/gtdb/data/releases/release226/226.0/ar53_metadata_r226.tsv.gz"
         }
-
         ## File check
         file_detection <- list.files()
         file_check <- default_files %in% file_detection
 
         if (!any(file_check)) {
                 cat("Downloading", domain, "metadata from GTDB...\n")
+                ## Trimming the database
                 data <- fread(download_url, header = TRUE)
+                cat("Reducing the database footprint...\n")
+                col_to_preserve <- c(
+                        "gtdb_taxonomy", "ncbi_taxonomy",
+                        "gtdb_representative", "ncbi_genbank_assembly_accession",
+                        "ncbi_taxid", "ncbi_species_taxid"
+                )
+                data <- subset(data, , col_to_preserve)
+                data <- tax_split(data)
+                data <- tax_split_ncbi(data)
                 local_file <- default_files[2] # Save as uncompressed TSV
                 fwrite(data, local_file, sep = "\t")
                 cat("Saved database to:", local_file, "\n")
@@ -144,7 +180,7 @@ if (!is.null(database_file)) {
 }
 
 ## NCBI dataset
-if (!file.exists("datasets")) {
+if (!file.exists("datasets") & !exists(quote(dataset_path))) {
         cat("Downloading NCBI datasets CLI tool...\n")
         download.file(
                 "https://ftp.ncbi.nlm.nih.gov/pub/datasets/command-line/v2/linux-amd64/datasets",
@@ -161,23 +197,54 @@ if (!file.exists("datasets")) {
 ## NOTE: 3. File input##
 ########################
 ## List prep
-processed_data <- tax_split(data)
-
+if (!("species" %in% colnames(data)) && !("ncbi_species" %in% colnames(data))) {
+        processed_data <- tax_split(data)
+        processed_data <- tax_split_ncbi(processed_data)
+} else {
+        processed_data <- data
+        rm(data)
+}
+## Tax id
+## Selection fallback taxrank > ncbi_species_taxid > ncbi_taxid
 selection <- processed_data[
-        get(taxrank) == name_selection & gtdb_representative == representative, c("ncbi_genbank_assembly_accession")
+        get(taxrank) == name_selection & gtdb_representative == representative,
+        c("ncbi_genbank_assembly_accession")
 ]
+if (nrow(selection) == 0) {
+        selection <- processed_data[
+                ncbi_taxid == name_selection & gtdb_representative == representative,
+                c("ncbi_genbank_assembly_accession")
+        ]
+        cat(yellow("No GTDB taxonomy name found falling back to NCBI taxonomical rank name\n"))
+        cat(yellow("For the optimal results please use GTDB taxonomy ranks\n"))
+}
+if (nrow(selection) == 0) {
+        selection <- processed_data[
+                ncbi_species_taxid == name_selection & gtdb_representative == representative,
+                c("ncbi_genbank_assembly_accession")
+        ]
+        cat(yellow("No taxonomical rank name found falling back to NCBI Species Taxid\n"))
+}
+if (nrow(selection) == 0) {
+        selection <- processed_data[
+                ncbi_taxid == name_selection & gtdb_representative == representative,
+                c("ncbi_genbank_assembly_accession")
+        ]
+        cat(yellow("No NCBI Species Taxid found falling back to NCBI Taxid\n"))
+}
 
 ## User confirmation and sample count check
 n_records <- nrow(selection)
-unique_species <- processed_data[, .N, by = get(taxrank)][order(-N)]
-colnames(unique_species) <- c("Name", "N")
-unique_species[, distance := adist(Name, name_selection)]
-setorder(unique_species, distance)
-top_similar <- unique_species[Name != name_selection][1:min(10, .N)]
-has_similar <- nrow(top_similar) > 0
 
 if (n_records == 0) {
+        ## Print similarity matrix if n_records are 0
         cat(red(paste0("\n", name_selection, " ", n_records, " records found.\n")))
+        unique_species <- processed_data[, .N, by = get(taxrank)][order(-N)]
+        colnames(unique_species) <- c("Name", "N")
+        unique_species[, distance := adist(Name, name_selection)]
+        setorder(unique_species, distance)
+        top_similar <- unique_species[Name != name_selection][1:min(10, .N)]
+        has_similar <- nrow(top_similar) > 0
         if (has_similar) {
                 cat("\nSimilar records:\n(Not selected for download)\n")
                 print(top_similar[, .(Name, Records = N)])
@@ -187,15 +254,13 @@ if (n_records == 0) {
         quit(status = 0)
 } else {
         cat(green(paste0("\n", name_selection, " ", n_records, " records found.\n")))
-        if (has_similar) {
-                cat("\nSimilar records:\n(Not selected for download)\n")
-                print(top_similar[, .(Name, Records = N)])
-        } else {
-                cat(red("No similar species found\n"))
-        }
 }
-
-cat("Would you like to proceed with the download? [y/n]: ")
+if (dryrun == TRUE) {
+        cat(yellow("Dryrun mode activated quitting"))
+        quit()
+} else {
+        cat("Would you like to proceed with the download? [y/n]: ")
+}
 if (non_interactive == TRUE) {
         print("Non interactive mode say yes")
 } else {
@@ -210,18 +275,30 @@ if (non_interactive == TRUE) {
 fwrite(selection, "download_accession_list.txt", col.names = FALSE)
 
 ## Genomes download
-system("./datasets download genome accession --inputfile download_accession_list.txt --dehydrated --include genome")
+if (!exists(quote(dataset_path))) {
+        dataset_path_input <- "./datasets "
+} else {
+        dataset_path_input <- paste0(dataset_path, " ")
+}
+
+system(paste0(dataset_path_input, "download genome accession --inputfile download_accession_list.txt --dehydrated --include genome"))
 unzip("ncbi_dataset.zip")
-system("./datasets rehydrate --directory .")
+system(paste0(dataset_path_input, "rehydrate --directory ."))
 dir.create(output_dir)
-file.rename(
+invisible(file.rename(
         from = list.files(path = "ncbi_dataset/data/", pattern = "\\.fna$", recursive = TRUE, full.names = TRUE),
         to = file.path(paste0("./", output_dir), basename(list.files("ncbi_dataset/data/", "\\.fna$", recursive = TRUE)))
-)
+))
+
+if (report == TRUE) {
+        setcolorder(processed_data, c("gtdb_taxonomy", "ncbi_taxonomy"), after = ncol(processed_data))
+        setcolorder(processed_data, "ncbi_genbank_assembly_accession", before = 1)
+        fwrite(processed_data[processed_data$ncbi_genbank_assembly_accession %in% selection$ncbi_genbank_assembly_accession, ], file = paste0(output_dir, "/download_report.csv"))
+}
 
 ## Cleanup
 rm_list <- c("download_accession_list.txt", "ncbi_dataset.zip", "md5sum.txt", "README.md")
-suppressMessages(file.remove(rm_list))
-suppressMessages(system("rm -r ncbi_dataset"))
+invisible(file.remove(rm_list))
+invisible(system("rm -r ncbi_dataset"))
 
 cat(green("Download completed"))
